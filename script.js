099ff98728b08696ca48c433bd64eb393c32e3b2
manishDev <idonlypanda@gmail.com>
2026-01-16 20:24:31 +0530

    INSTA story page

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-var con = document.querySelector("#container");
-var love = document.querySelector("i");
+var rect = document.querySelector("#center");
 
-con.addEventListener("dblclick",function(){
-  love.style.transform = 'translate(-50%,-50%) scale(1)';
-  love.style.opacity = 0.8;
-  
-  setTimeout(()=> {
-    love.style.opacity = 0;
-  },1000);
-  setTimeout(function(){
-    love.style.transform = 'translate(-50%, -50%) scale(0)';
-  },800);
-})
+//On Mouse Enter -> remove gradient so GSAP can work again
+rect.addEventListener("mouseenter", function(){
+  rect.style.background = ""; //remove gradient
+});
+
+rect.addEventListener("mousemove", function(details){
+  var rectangleLocation = rect.getBoundingClientRect();
+  var insiderectval = details.clientX - rectangleLocation.left;
+
+  if(insiderectval < rectangleLocation.width/2){
+    var redcolor = gsap.utils.mapRange(
+      0,
+      rectangleLocation.width/2,
+      255,
+      0,
+      insiderectval
+    );
+    gsap.to(rect, {
+      backgroundColor: `rgb(${redcolor},0,0)`,
+      ease: Power4,
+    })
+  }else{
+    var bluecolor = gsap.utils.mapRange(
+      rectangleLocation.width/2,
+      rectangleLocation.width,
+      0,
+      255,
+      insiderectval
+    );
+    gsap.to(rect, {
+      backgroundColor: `rgb(0,0,${bluecolor})`,
+      ease: Power4,
+    })
+  }
+});
+
+rect.addEventListener("mouseleave",function(){
+  gsap.to(rect, {
+    background: "linear-gradient(to bottom, #FF9933, #FFFFFF, #138808)",
+    ease: Power4,
+    duration: 0.8
+  });
+});
