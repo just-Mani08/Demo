ba6aa4793bcff8ab3c72467887acd1a5cec2d103
manishDev <idonlypanda@gmail.com>
2026-01-16 20:21:44 +0530

    INSTA add friend page

--- script.js
+++ script.js
@@ -1,50 +1,19 @@
-var timer = 60;
-var score = 0;
-var hitrn = 0;
+// Select elements
+var istatus = document.querySelector("#card h5");
+var addBtn = document.querySelector("#add");
 
-function increaseScore(){
-  score += 10;
-  document.querySelector("#scoreVal").textContent = score;
-}
+let check = 0;
 
-function makeBubble(){
-  var clutter = "";
-  
-  for(var i = 1;i<= 168;i++){
-  var rn = Math.floor(Math.random() * 10);
-  clutter += `<div class="bubble">${rn}</div>`
-}
-
-document.querySelector("#pbtm").innerHTML = clutter;
-}
-
-function getNewHit(){
-  hitrn = Math.floor(Math.random()*10);
-  document.querySelector("#hitVal").textContent = hitrn;
-}
-
-function runTimer() {
-  var interval = setInterval(() => {
-    timer--;
-    document.querySelector("#timeVal").textContent = timer;
-    if(timer <= 0){
-      clearInterval(interval);
-      document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
-    }
-  }, 1000);
-}
-
-//bubbling CONCEPT = Jab main kisi bubble pe click karunga tab ek event listen hoga, agar woh nhi hua toh woh uske parent par event listen uspe bhi nhi hua toh uske bhi parent pe hoga
-
-document.querySelector("#pbtm").addEventListener("click", function(details){
-  var clickedNum = Number(details.target.textContent);
-  if(clickedNum === hitrn){
-    increaseScore();
-    makeBubble();
-    getNewHit();
+addBtn.addEventListener("click", function () {
+  if (check === 0) {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+    addBtn.innerHTML = "Remove Friend";
+    check = 1;
+  } else {
+    istatus.innerHTML = "Stranger";
+    istatus.style.color = "red";
+    addBtn.innerHTML = "Add Friend";
+    check = 0;
   }
 });
-
-makeBubble();
-getNewHit();
-runTimer();
