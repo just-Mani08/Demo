63206355cbe9483f8831341bce8e5a90c0c9de30
manishDev <idonlypanda@gmail.com>
2026-01-16 20:23:12 +0530

    INSTA like

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var main = document.querySelector("#main")
-var cursor = document.querySelector(".cursor")
+var con = document.querySelector("#container");
+var love = document.querySelector("i");
 
-main.addEventListener("mousemove", function(dets){
-  //These left and will only work when you give the positon: absolute; to the cursor in css
-  cursor.style.left = dets.x+"px"; 
-  cursor.style.top = dets.y+"px";
-})
-h1.addEventListener("mouseenter",function(){
-  cursor.style.transform += "scale(2)";
-})
-h1.addEventListener("mouseleave",function(){
-  cursor.style.transform = "translate(-50%,-50%)";
+con.addEventListener("dblclick",function(){
+  love.style.transform = 'translate(-50%,-50%) scale(1)';
+  love.style.opacity = 0.8;
+  
+  setTimeout(()=> {
+    love.style.opacity = 0;
+  },1000);
+  setTimeout(function(){
+    love.style.transform = 'translate(-50%, -50%) scale(0)';
+  },800);
 })
