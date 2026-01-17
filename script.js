506bc56a453309e73f76243c101237e35e5e7f2b
manishDev <idonlypanda@gmail.com>
2026-01-18 00:34:47 +0530

    Quiz

--- script.js
+++ script.js
@@ -1,45 +1,123 @@
-var rect = document.querySelector("#center");
+const questions = [
+  {
+    question : "Which is the largest animal in the world?",
+    answers : [
+      {text: "Shark", correct: false},
+      {text: "Blue whale",correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false}
+    ]
+  },
+    {
+      question : "Which is the smallest country in the world?",
+    answers : [
+      {text: "Vatican city", correct: true},
+      {text: "Bhutan", correct: false},
+      {text: "Nepal", correct: false},
+      {text: "Shri Lanka", correct: false}
+    ]
+    },
+    {
+      question : "Which is the largest desert in the world?",
+      answers: [
+        {text: "Kalahari", correct: false},
+        {text: "Gobi", correct: false},
+        {text: "Sahara", correct: false},
+        {text: "Antarctica", correct: true}
+      ]      
+    },
+    {
+      question : "Which is the smallest continent in the world?",
+      answers: [
+        {text: "Asia", correct: false},
+        {text: "Australia", correct: true},
+        {text: "Arctic", correct: false},
+        {text: "Africa", correct: false}
+      ]
+    }  
+];
 
-//On Mouse Enter -> remove gradient so GSAP can work again
-rect.addEventListener("mouseenter", function(){
-  rect.style.background = ""; //remove gradient
-});
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn");
 
-rect.addEventListener("mousemove", function(details){
-  var rectangleLocation = rect.getBoundingClientRect();
-  var insiderectval = details.clientX - rectangleLocation.left;
+let currentQuestionIndex = 0;
+let score = 0;
 
-  if(insiderectval < rectangleLocation.width/2){
-    var redcolor = gsap.utils.mapRange(
-      0,
-      rectangleLocation.width/2,
-      255,
-      0,
-      insiderectval
-    );
-    gsap.to(rect, {
-      backgroundColor: `rgb(${redcolor},0,0)`,
-      ease: Power4,
-    })
+function startQuiz(){
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "next";
+  showQuestion();
+}
+
+function showQuestion(){
+  resetState();
+
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click",selectAnswer);
+  })
+}
+function resetState(){
+  nextButton.style.display = "none";
+  while(answerButtons.firstChild){
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+function selectAnswer(e){
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+    score++;
   }else{
-    var bluecolor = gsap.utils.mapRange(
-      rectangleLocation.width/2,
-      rectangleLocation.width,
-      0,
-      255,
-      insiderectval
-    );
-    gsap.to(rect, {
-      backgroundColor: `rgb(0,0,${bluecolor})`,
-      ease: Power4,
-    })
+    selectedBtn.classList.add("incorrect");
+  }
+
+  //Disable all buttons and show correct answer
+  Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore(){
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex < questions.length){
+    showQuestion();
+  }else{
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", ()=> {
+  if(currentQuestionIndex < questions.length){
+    handleNextButton();
+  }
+  else{
+    startQuiz();
   }
 });
-
-rect.addEventListener("mouseleave",function(){
-  gsap.to(rect, {
-    background: "linear-gradient(to bottom, #FF9933, #FFFFFF, #138808)",
-    ease: Power4,
-    duration: 0.8
-  });
-});
+startQuiz();
